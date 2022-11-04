720709c130b1ae6099cbdf1c798aea15001a48fe
Shuying Liu <shuyingliu24@gmail.com>
2022-11-04 01:16:42 +0000

    modified V

--- proj3.R
+++ proj3.R
@@ -9,7 +9,8 @@
 # i.e. estimate the smooth function f in model: y=f(x)+e.
 # We will use a pspline fit to estimate f: Use basis expansion
 # f=sum(beta_jb_j) where b_i are k evenly spaced Bspline basis functions, so the
-# model will be turned into a linear model y=Xbeta+e, where X_ij=b_j(x_i).
+# model will be turned into a linear model y=Xbeta+e, where X_ij=b_j(x_i),
+# and beta is the vector of coefficients of basis functions.
 # And we impose a penalty, for example order 2 difference
 # sum(beta_{i-1}-2beta_i+beta_{i+1})=beta^TD^TDbeta
 # to avoid over-fitting, then estimate the linear model by penalized least squares 
@@ -82,19 +83,19 @@ pspline <- function(x,y,k=20,logsp=c(-5,5),bord=3,pord=2,ngrid=100) {
   sig2 <- colSums((y-fitted)^2)/(n-edf) # estimated residual variance
   gcv <- sig2/(n-edf) # generalized cross validation
 
-  # Find the lambda minimizing GCV
+  # Find the lambda minimizing GCV to get the best fit
   lambda_i <- which.min(gcv)
   diag_lambda_fit <- 1/(1+lambda[lambda_i]*Lambda)
   
   # Get details about the best fit
   sig2_fit <- sig2[lambda_i] # estimated residual variance
   V_fit <- sig2_fit*RU %*% (diag_lambda_fit * t(RU)) # covariance matrix of beta
-  r2_fit <- 1-(n-1)*sig2_fit/sum((y-mean(y))^2)# r-squared
+  r2_fit <- 1-(n-1)*sig2_fit/sum((y-mean(y))^2) # r-squared
   
   smoother <- list(data=cbind(x,y),k=k,ord=c(bord,pord),
                    knots=knots,coef=coef[,lambda_i],
                    fitted=fitted[,lambda_i],sig2=sig2_fit,edf=edf[lambda_i],
-                   rse=sqrt(sig2_fit),r2=r2_fit,gcv=gcv[lambda_i],cov=V_fit)
+                   r2=r2_fit,gcv=gcv[lambda_i],cov=V_fit)
   class(smoother) <- "pspline"
   
   return(smoother)
@@ -110,7 +111,7 @@ print.pspline <- function(m) {
   l2 <- sprintf("Effective degrees of freedom: %.7g\tCoefficients: %d",
                 m$edf,m$k)
   l3 <- sprintf("residual std dev: %.7g\tr-squared: %.7g\tGCV: %.7g",
-                m$rse,m$r2,m$gcv)
+                m$sig2^.5,m$r2,m$gcv)
   cat(l1,l2,l3,sep='\n')
   
   invisible(list(gcv=m$gcv,edf=m$edf,r2=m$r2))
@@ -131,7 +132,8 @@ predict.pspline <- function(m,x,se=TRUE) {
   if (se) {
     # The  required standard errors come from the
     # square roots of the leading diagonals 
-    # of the predicted value covariance matrix
+    # of the predicted value covariance matrix X_pVX_p^T,
+    # where V is  the covariance matrix for the coefficients
     return(list(fit=fitted,se=rowSums(Xp * (Xp %*% m$cov))^.5))
   }
   else return(fitted)
@@ -145,6 +147,8 @@ plot.pspline <- function(m) {
   #   approximate 95% credible intervals for the smooth
   #   Second plot is the model residuals against fitted values
   #   Third plot is the qqplot of the residuals
+  # Return a list containing the lower and upper confidence limits ll and ul
+  # and the corresponding x values
   
   x <- m$data[,1]
   y <- m$data[,2]
@@ -182,7 +186,7 @@ plot.pspline <- function(m) {
 
 
 test_example <- function() {
-  # Test using data from MASS mcycle
+  # Test with data from MASS mcycle
   
   library(MASS)
   x <- mcycle[,1]
